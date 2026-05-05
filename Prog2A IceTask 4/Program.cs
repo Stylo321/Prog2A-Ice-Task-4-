ee6466a66bb8f39ab1b5b87329835b054e50f663
Stylo <165194260+Stylo321@users.noreply.github.com>
2026-05-05 18:50:04 +0200

    Add files via upload

---
+++ Prog2A IceTask 4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    // Delegate
+    delegate int DoubleNumber(int x);
+
+    static void Main(string[] args)
+    {
+        // a) Anonymous Method
+        DoubleNumber doublerAnonymous = delegate (int x)
+        {
+            return x * 2;
+        };
+
+        Console.WriteLine("Anonymous Method Result: " + doublerAnonymous(5));
+
+        // b) Lambda Expression
+        DoubleNumber doublerLambda = x => x * 2;
+
+        Console.WriteLine("Lambda Expression Result: " + doublerLambda(5));
+    }
+}
